7f357c0b0c034ff6b14a108c369f11a7461c139a
Bergamo <gabrielpb2@al.insper.edu.br>
2025-09-28 21:40:44 -0300

    bssns

--- steeple/src/main/java/br/edu/insper/desagil/aps3/steeple/Defender.java
+++ steeple/src/main/java/br/edu/insper/desagil/aps3/steeple/Defender.java
@@ -2,6 +2,10 @@ package br.edu.insper.desagil.aps3.steeple;
 
 public class Defender extends Unit {
     public Defender(int x, int y, int power, int health) {
+
+        private String c;
+
+
         super(x, y, power, health);
     }
 }
